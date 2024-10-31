a434b5f05634fdd74d87cde33336d256cb98e91c
Rebelstar <145977706+Jaygiri07@users.noreply.github.com>
2024-11-01 00:43:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002314866251'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002212387855'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6717382350').split()]
 
 # MongoDB information
